b4297d01ad8cefe9b72f4ea8668408d765846adb
ToadsworthLP <fiery.toadsworth@gmail.com>
2024-11-07 00:38:36 +0100

    Added new texture for Chara

--- Content/FallenHumanProjectile.cs
+++ Content/FallenHumanProjectile.cs
@@ -66,8 +66,8 @@ public class FallenHumanProjectile : ModProjectile, IStateMachineTarget
 		ProjectileID.Sets.TrailingMode[Projectile.type] = 0;
 		
 		ProjectileID.Sets.CharacterPreviewAnimations[Projectile.type] = ProjectileID.Sets.SimpleLoop(0, Main.projFrames[Projectile.type], 6)
-			.WithOffset(-10, 0f)
-			.WithSpriteDirection(-1)
+			.WithOffset(0, -6f)
+			.WithSpriteDirection(1)
 			.WithCode(PreviewAnimation);
 
 		EnemyDetector = new EnemyDetector(500f);
@@ -93,12 +93,12 @@ public class FallenHumanProjectile : ModProjectile, IStateMachineTarget
 			proj.position.Y += 0f - num + (float)(Math.Cos(num2 * ((float)Math.PI * 2f) * 2f * 0.5f) * (num * 2f));
 		}
 		
-		proj.scale = 0.8f;
+		proj.scale = 1.0f;
 	}
 
 	public override void SetDefaults() {
-		Projectile.width = 38;
-		Projectile.height = 54;
+		Projectile.width = 24;
+		Projectile.height = 40;
 		Projectile.penetrate = -1;
 		Projectile.netImportant = true;
 		Projectile.timeLeft *= 5;
@@ -164,90 +164,4 @@ public class FallenHumanProjectile : ModProjectile, IStateMachineTarget
 			LastStateId = lastStateId;
 		}
 	}
-
-	// private void UpdateDash(Player player) {
-	// 	// The following code makes our pet dash at enemies when certain conditions are met
-	//
-	// 	AIDashCharge++;
-	//
-	// 	if (AIDashCharge <= DashCooldown || (int)AIFadeProgress % 100 != 0) {
-	// 		return;
-	// 	}
-	//
-	// 	// Enumerate
-	// 	foreach (var npc in Main.ActiveNPCs) {
-	// 		// Ignore this npc if it's friendly.
-	// 		if (npc.friendly) {
-	// 			continue;
-	// 		}
-	//
-	// 		// Ignore this npc if it's too far away. Note that we're using squared values for our checks, to avoid square root calculations as a small, but effective optimization.
-	// 		if (player.DistanceSQ(npc.Center) >= RangeHypotenuseSquared) {
-	// 			continue;
-	// 		}
-	//
-	// 		Projectile.velocity += Vector2.Normalize(npc.Center - Projectile.Center) * DashSpeed; // Fling the projectile towards the npc.
-	// 		AIDashCharge = 0f; // Reset the charge.
-	//
-	// 		// Play a sound.
-	// 		if (!Main.dedServ) {
-	// 			SoundEngine.PlaySound(SoundID.Item42, Projectile.Center);
-	// 		}
-	//
-	// 		break;
-	// 	}
-	// }
-
-	// private void UpdateFading(Player player) {
-	// 	//TODO: Comment and clean this up more.
-	//
-	// 	var playerCenter = player.Center; // Cache the player's center vector to avoid recalculations.
-	//
-	// 	AIFadeProgress++;
-	//
-	// 	if (AIFadeProgress < FadeInTicks) {
-	// 		Projectile.alpha = (int)(255 - 255 * AIFadeProgress / FadeInTicks);
-	// 	}
-	// 	else if (AIFadeProgress < FadeInTicks + FullBrightTicks) {
-	// 		Projectile.alpha = 0;
-	//
-	// 		if (Main.rand.NextBool(6)) {
-	// 			var dust = Dust.NewDustDirect(Projectile.position, Projectile.width, Projectile.height, DustID.PinkFairy, 0f, 0f, 200, default, 0.8f);
-	//
-	// 			dust.velocity *= 0.3f;
-	// 		}
-	// 	}
-	// 	else if (AIFadeProgress < FadeInTicks + FullBrightTicks + FadeOutTicks) {
-	// 		Projectile.alpha = (int)(255 * (AIFadeProgress - FadeInTicks - FullBrightTicks) / FadeOutTicks);
-	// 	}
-	// 	else {
-	// 		Projectile.Center = playerCenter + Main.rand.NextVector2Circular(Range, Range);
-	// 		AIFadeProgress = 0f;
-	//
-	// 		Projectile.velocity = 2f * Vector2.Normalize(playerCenter - Projectile.Center);
-	// 	}
-	//
-	// 	if (Vector2.Distance(playerCenter, Projectile.Center) > RangeHypotenuse) {
-	// 		Projectile.Center = playerCenter + Main.rand.NextVector2Circular(Range, Range);
-	// 		AIFadeProgress = 0f;
-	//
-	// 		Projectile.velocity += 2f * Vector2.Normalize(playerCenter - Projectile.Center);
-	// 	}
-	//
-	// 	if ((int)AIFadeProgress % 100 == 0) {
-	// 		Projectile.velocity = Projectile.velocity.RotatedByRandom(MathHelper.ToRadians(90));
-	// 	}
-	// }
-
-	// private void UpdateExtraMovement() {
-	// 	// Adds some friction to the pet's movement as long as its speed is above 1
-	// 	if (Projectile.velocity.Length() > 1f) {
-	// 		Projectile.velocity *= 0.98f;
-	// 	}
-	//
-	// 	// If the pet stops - launch it into a random direction at a low speed.
-	// 	if (Projectile.velocity == Vector2.Zero) {
-	// 		Projectile.velocity = Vector2.UnitX.RotatedBy(Main.rand.NextFloat() * MathHelper.TwoPi) * 2f;
-	// 	}
-	// }
 }
